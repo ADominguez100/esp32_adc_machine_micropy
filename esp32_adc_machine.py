02ed075cbfbe8669ed9ad396d2a6a241d3c8cdad
adria <adomingue3@inscamidemar.cat>
2025-10-20 21:11:13 +0200

    original file added

---
+++ esp32_adc_machine.py
@@ -0,0 +1,10 @@
+#Write a Micropython script to read the analog data provided by POT connected  to GPIO34 and print on shell every o.1 sec
+from machine import Pin, ADC
+from time import sleep
+POT = ADC(Pin(34))
+
+POT.atten(ADC.ATTN_11DB)#Specifying the voltage of the  Analog sensor
+while True:
+    pot_value  = POT.read()
+    print(pot_value)
+    sleep(0.1)
